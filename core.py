73acf5a1e648363cdd6e4115953ad318f203b29c
masazdream <masaz.dream@gmail.com>
2016-12-06 22:04:57 +0900

    add several parameters.

--- core.py
+++ core.py
@@ -40,12 +40,31 @@ def clahe_hsv(image_path, verbose=False):
     return bgr
 
 
-def clahe(image_path, denoise=False, verbose=False, limit=None):
+def clahe_inv(image_path, denoise=False, verbose=False, limit=None):
     bgr = cv2.imread(image_path)
 
     if denoise:
-        #bgr = cv2.fastNlMeansDenoisingColoredMulti([bgr, bgr, bgr, bgr, bgr], 2, 5, None, 4, 5, 35)
-        bgr = cv2.fastNlMeansDenoisingColored(bgr, None, 10, 10, 7, 21)
+        bgr = cv2.fastNlMeansDenoisingColored(bgr, None, 10, 10, 1, 3)
+        # bgr = cv2.bilateralFilter(bgr, 5, 1, 1)
+
+    lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
+    lab_planes = cv2.split(lab)
+    clahe = cv2.createCLAHE(clipLimit=limit)
+    lab_planes[0] = clahe.apply(lab_planes[0])
+    # lab_planes[1] = clahe.apply(lab_planes[1])
+    # lab_planes[2] = clahe.apply(lab_planes[2])
+    lab = cv2.merge(lab_planes)
+    bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+
+    if verbose:
+        cv2.imshow("test", bgr)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    return bgr
+
+
+def clahe(image_path, denoise=False, verbose=False, limit=None):
+    bgr = cv2.imread(image_path)
 
     lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
     lab_planes = cv2.split(lab)
@@ -55,10 +74,35 @@ def clahe(image_path, denoise=False, verbose=False, limit=None):
     #lab_planes[2] = clahe.apply(lab_planes[2])
     lab = cv2.merge(lab_planes)
     bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
-    #
-    # if denoise:
-    #     #bgr = cv2.fastNlMeansDenoisingColoredMulti([bgr, bgr, bgr, bgr, bgr], 2, 5, None, 4, 5, 35)
-    #     bgr = cv2.fastNlMeansDenoisingColored(bgr, None, 10, 10, 7, 21)
+
+    if denoise:
+        bgr = cv2.fastNlMeansDenoisingColored(bgr, None, 10, 10, 1, 3)
+        #bgr = cv2.bilateralFilter(bgr, 5, 1, 1)
+
+    if verbose:
+        cv2.imshow("test", bgr)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    return bgr
+
+def b_clahe_nlm(image_path, denoise=False, verbose=False, limit=None):
+    bgr = cv2.imread(image_path)
+
+    if denoise:
+        bgr = cv2.bilateralFilter(bgr, 3, 3, 2)
+
+    lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
+    lab_planes = cv2.split(lab)
+    clahe = cv2.createCLAHE(clipLimit=limit)
+    lab_planes[0] = clahe.apply(lab_planes[0])
+    #lab_planes[1] = clahe.apply(lab_planes[1])
+    #lab_planes[2] = clahe.apply(lab_planes[2])
+    lab = cv2.merge(lab_planes)
+    bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+
+    if denoise:
+        bgr = cv2.fastNlMeansDenoisingColored(bgr, None, 10, 10, 3, 9)
+        #bgr = cv2.bilateralFilter(bgr, 5, 1, 1)
 
     if verbose:
         cv2.imshow("test", bgr)
@@ -67,7 +111,52 @@ def clahe(image_path, denoise=False, verbose=False, limit=None):
     return bgr
 
 
-def convert(image_dir, output_home, option="clahe", limit=None):
+
+def clahe_inv_b(image_path, denoise=False, verbose=False, limit=None):
+    bgr = cv2.imread(image_path)
+
+    if denoise:
+        bgr = cv2.bilateralFilter(bgr, 15, 5, 2)
+
+    lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
+    lab_planes = cv2.split(lab)
+    clahe = cv2.createCLAHE(clipLimit=limit)
+    lab_planes[0] = clahe.apply(lab_planes[0])
+    # lab_planes[1] = clahe.apply(lab_planes[1])
+    # lab_planes[2] = clahe.apply(lab_planes[2])
+    lab = cv2.merge(lab_planes)
+    bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+
+    if verbose:
+        cv2.imshow("test", bgr)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    return bgr
+
+
+def clahe_b(image_path, denoise=False, verbose=False, limit=None):
+    bgr = cv2.imread(image_path)
+
+    lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
+    lab_planes = cv2.split(lab)
+    clahe = cv2.createCLAHE(clipLimit=limit)
+    lab_planes[0] = clahe.apply(lab_planes[0])
+    #lab_planes[1] = clahe.apply(lab_planes[1])
+    #lab_planes[2] = clahe.apply(lab_planes[2])
+    lab = cv2.merge(lab_planes)
+    bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+
+    if denoise:
+        bgr = cv2.bilateralFilter(bgr, 15, 5, 2)
+
+    if verbose:
+        cv2.imshow("test", bgr)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    return bgr
+
+
+def convert(image_dir, output_home, option="clahe", limit=None, verbose=False):
     list = os.listdir(image_dir)
     for f in list:
         if "jpg" in f:
@@ -81,21 +170,60 @@ def convert(image_dir, output_home, option="clahe", limit=None):
                 bgr = clahe_hsv(image_path)
             elif option == "eql_de":
                 bgr = equlizehist(image_path, denoise=True)
+            elif option == "best":
+                bgr = b_clahe_nlm(image_path, denoise=True, limit=limit)
+            elif option == "all":
+                bgr = b_clahe_nlm(image_path, denoise=True, limit=limit)
+                bgr_b = clahe_b(image_path, denoise=True, limit=limit)
+                bgr_inv = clahe_inv(image_path, denoise=True, limit=limit)
+                bgr_inv_b = clahe_inv_b(image_path, denoise=True, limit=limit)
+
+                bgr_2 = clahe(image_path, denoise=True, limit=2)
+                bgr_b_2 = clahe_b(image_path, denoise=True, limit=2)
+                bgr_inv_2 = clahe_inv(image_path, denoise=True, limit=2)
+                bgr_inv_b_2 = clahe_inv_b(image_path, denoise=True, limit=2)
+
+                vis = np.concatenate((bgr, bgr_b, bgr_inv, bgr_inv_b), axis=1)
+                vis_2 = np.concatenate((bgr_2, bgr_b_2, bgr_inv_2, bgr_inv_b_2), axis=1)
+
+                v = np.concatenate((vis, vis_2), axis=0)
+
+                font = cv2.FONT_HERSHEY_PLAIN
+                cv2.putText(v, "CLAHE_NLM", (10, 100), font, 3, (0, 0, 255))
+                cv2.putText(v, "CLAHE_BI", (650, 100), font, 3, (0, 0, 255))
+                cv2.putText(v, "NLM_CLAHE", (1300, 100), font, 3, (0, 0, 255))
+                cv2.putText(v, "BI_CLAHE", (1950, 100), font, 3, (0, 0, 255))
+
+                cv2.putText(v, "CLAHE_NLM_2", (10, 590), font, 3, (0, 0, 255))
+                cv2.putText(v, "CLAHE_2_BI", (650, 590), font, 3, (0, 0, 255))
+                cv2.putText(v, "NLM_CLAHE_2", (1300, 590), font, 3, (0, 0, 255))
+                cv2.putText(v, "BI_CLAHE_2", (1950, 590), font, 3, (0, 0, 255))
+
+                basename = os.path.basename(image_dir)
+                output_dir = os.path.join(output_home, basename)
+                if not os.path.isdir(output_dir):
+                    os.mkdir(output_dir)
+                output_file = os.path.join(output_dir, f)
+                print output_file
+                cv2.imwrite(output_file, v)
+
             else:
                 bgr = equlizehist(image_path)
-            basename = os.path.basename(image_dir)
-            output_dir = os.path.join(output_home, basename)
-            if not os.path.isdir(output_dir):
-                os.mkdir(output_dir)
-            output_file = os.path.join(output_dir, f)
-            cv2.imwrite(output_file, bgr)
+
+            if not option == "all":
+                basename = os.path.basename(image_dir)
+                output_dir = os.path.join(output_home, basename)
+                if not os.path.isdir(output_dir):
+                    os.mkdir(output_dir)
+                output_file = os.path.join(output_dir, f)
+                cv2.imwrite(output_file, bgr)
 
 if __name__ == '__main__':
     argvs = sys.argv
     argc = len(argvs)
 
     if argc < 4:
-        print("[Usage]python %s <option clahe, clahe_de, clahe_hsv, eql, eql_de> <image home dir> <output home dir> <limit>" % (argvs[0]))
+        print("[Usage]python %s <option clahe, clahe_de, clahe_hsv, eql, eql_de, best, all> <image home dir> <output home dir> <limit>" % (argvs[0]))
         sys.exit(-1)
 
     option = argvs[1]
@@ -107,7 +235,7 @@ if __name__ == '__main__':
         image_dir_path = os.path.join(image_home_dir, dir)
         if not os.path.isdir(image_dir_path):
             continue
-        convert(image_dir_path, output_home_dir, option=option, limit=int(limit))
+        convert(image_dir_path, output_home_dir, option=option, limit=int(limit), verbose=True)
 
     # equlizehist()
     # clahe()
