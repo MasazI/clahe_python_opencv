2c7b7abaacafff0ef314f8036722a73989261ad8
masazdream <masaz.dream@gmail.com>
2016-12-06 12:50:50 +0900

    denoize

--- core.py
+++ core.py
@@ -3,6 +3,7 @@ import sys
 import os
 import os.path
 import cv2
+import numpy as np
 
 
 def equlizehist(image_path, verbose=False):
@@ -17,7 +18,24 @@ def equlizehist(image_path, verbose=False):
     return bgr
 
 
-def clahe(image_path, verbose=False):
+def clahe_hsv(image_path, verbose=False):
+    bgr = cv2.imread(image_path)
+    hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+    hsv_planes = cv2.split(hsv)
+    clahe = cv2.createCLAHE(clipLimit=4)
+    hsv_planes[0] = clahe.apply(hsv_planes[2])
+    #lab_planes[1] = clahe.apply(lab_planes[1])
+    #lab_planes[2] = clahe.apply(lab_planes[2])
+    lab = cv2.merge(hsv_planes)
+    bgr = cv2.cvtColor(lab, cv2.COLOR_HSV2BGR)
+    if verbose:
+        cv2.imshow("test", bgr)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    return bgr
+
+
+def clahe(image_path, denoise=False, verbose=False):
     bgr = cv2.imread(image_path)
     lab = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
     lab_planes = cv2.split(lab)
@@ -27,6 +45,11 @@ def clahe(image_path, verbose=False):
     #lab_planes[2] = clahe.apply(lab_planes[2])
     lab = cv2.merge(lab_planes)
     bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+
+    if denoise:
+        #bgr = cv2.fastNlMeansDenoisingColoredMulti([bgr, bgr, bgr, bgr, bgr], 2, 5, None, 4, 5, 35)
+        bgr = cv2.fastNlMeansDenoisingColored(bgr, None, 10, 10, 7, 21)
+
     if verbose:
         cv2.imshow("test", bgr)
         cv2.waitKey(0)
@@ -38,9 +61,14 @@ def convert(image_dir, output_home, option="clahe"):
     list = os.listdir(image_dir)
     for f in list:
         if "jpg" in f:
+            print("processing: %s" % (f))
             image_path = os.path.join(image_dir, f)
             if option == "clahe":
                 bgr = clahe(image_path)
+            elif option == "clahe_de":
+                bgr = clahe(image_path, denoise=True)
+            elif option == "clahe_hsv":
+                bgr = clahe_hsv(image_path)
             else:
                 bgr = equlizehist(image_path)
             basename = os.path.basename(image_dir)
@@ -55,14 +83,20 @@ if __name__ == '__main__':
     argc = len(argvs)
 
     if argc < 3:
-        print("[Usage]python %s <option clahe or eql> <image dir> <output home dir>" % (argvs[0]))
+        print("[Usage]python %s <option clahe, clahe_de, clahe_hsv, eql> <image home dir> <output home dir>" % (argvs[0]))
         sys.exit(-1)
 
     option = argvs[1]
-    image_dir = argvs[2]
+    image_home_dir = argvs[2]
     output_home_dir = argvs[3]
 
-    convert(image_dir, output_home_dir, option=option)
+    list_dir = os.listdir(image_home_dir)
+
+    for dir in list_dir:
+        image_dir_path = os.path.join(image_home_dir, dir)
+        if not os.path.isdir(image_dir_path):
+            continue
+        convert(image_dir_path, output_home_dir, option=option)
 
     # equlizehist()
     # clahe()
